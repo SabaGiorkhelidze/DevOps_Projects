b553cb21e567c92276ee52ca9617ba9cb222ea72
SabaGiorkhelidze <sabagiorkhelidze@gmail.com>
2025-04-07 14:20:39 +0000

    first commit

---
+++ scripts/Server Stats/server-stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# server-stats.sh
+# A script to analyze basic server performance statistics
+
+echo "--------------------------------------------------"
+echo "🖥️  SERVER PERFORMANCE STATS"
+echo "Generated on: $(date)"
+echo "--------------------------------------------------"
